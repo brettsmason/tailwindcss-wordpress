037d7c7c3930a3231393aa11fb6b1635c7bd9592
Brett Mason <brettsmason@gmail.com>
2019-08-10 09:20:01 +0100

    Add required utilities

---
+++ README.md
@@ -0,0 +1,56 @@
+# Tailwind CSS WordPress Utility Classes
+
+Tailwind CSS plugin for WordPress specific utility classes.
+
+This plugin currently includes:
+
+- Editor Color Palette classes (`.has-[color]-text-color` and `.has-[color]-background-class`)
+- Editor Font Size classes (`.has-[size]-font-size`)
+- `.screen-reader-text` - WordPress specific accessibility class
+
+## Install
+
+1. Install the plugin:
+
+  ```bash
+  # Using npm
+  npm install tailwindcss-wordpress --save-dev
+
+  # Using Yarn
+  yarn add tailwindcss-wordpress -D
+  ```
+
+2. Add it to your `tailwind.config.js` file:
+
+  ```js
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      require('tailwindcss-wordpress')
+    ]
+  }
+  ```
+
+## Usage
+
+This plugin uses the keys `editorColorPalette` and `editorFontSizes` in your Tailwind config’s `theme` object to generate the utilities. Here is an example:
+
+```js
+// tailwind.config.js
+{
+  theme: {
+    editorColorPalette: {
+      'primary': '#f2f2f2',
+      'secondary': '#ccc',
+    },
+    editorFontSizes: {
+      'small': '16px',
+      'medium': '22px',
+    },
+  },
+  plugins: [
+    require('tailwindcss-wordpress')(),
+  ],
+}
+```
--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-module.exports = function({ addUtilities }) {
-  const WordPressUtilities = {
-    '.screen-reader-text': {
-      border: '0',
-      clip: 'rect(1px, 1px, 1px, 1px)',
-      clipPath: 'inset(50%)',
-      height: '1px',
-      margin: '-1px',
-      overflow: 'hidden',
-      padding: '0',
-      position: 'absolute',
-      width: '1px',
-      wordWrap: 'normal !important',
-      '&:focus': {
-        position: 'static',
-        width: 'auto',
-        height: 'auto',
-        overflow: 'visible',
-        clip: 'auto',
-        whiteSpace: 'normal'
+const _ = require('lodash');
+
+module.exports = function({ addUtilities, theme }) {
+  const editorColorText = _.map(
+    theme("editorColorPalette", {}),
+    (value, key) => {
+      return {
+        [`.has-${key}-text-color`]: {
+          color: value
+        },
+      };
+    }
+  );
+
+  const editorColorBackground = _.map(
+    theme("editorColorPalette", {}),
+    (value, key) => {
+      return {
+        [`.has-${key}-background-color`]: {
+          backgroundColor: value
+        },
+      };
+    }
+  );
+
+  const editorFontSizes = _.map(
+    theme("editorFontSizes", {}),
+    (value, key) => {
+      return {
+        [`.has-${key}-font-size`]: {
+          fontSize: value
+        }
+      };
+    }
+  );
+
+  const screenReaderText = {
+    ".screen-reader-text": {
+      border: "0",
+      clip: "rect(1px, 1px, 1px, 1px)",
+      clipPath: "inset(50%)",
+      height: "1px",
+      margin: "-1px",
+      overflow: "hidden",
+      padding: "0",
+      position: "absolute",
+      width: "1px",
+      wordWrap: "normal !important",
+      "&:focus": {
+        position: "static",
+        width: "auto",
+        height: "auto",
+        overflow: "visible",
+        clip: "auto",
+        whiteSpace: "normal"
       }
     }
-  }
+  };
 
-  addUtilities(WordPressUtilities)
+  addUtilities([
+    editorColorText,
+    editorColorBackground,
+    editorFontSizes,
+    screenReaderText
+  ], {
+    respectPrefix: false,
+    respectImportant: false,
+  });
 }
